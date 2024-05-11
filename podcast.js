2339e2329660714dd581eb7e9d3056fc6d006be7
Allen Astral De Jesus <dj.allenastral@gmail.com>
2024-05-11 23:29:57 +0800

    podcast: assets, js, hero section

---
+++ podcast.js
@@ -0,0 +1,22 @@
+const jsonData = [
+  {
+    imgUrl: "./assets/tesla-autopilot.png",
+    title: "Tesla Autopilot",
+  },
+  {
+    imgUrl: "./assets/self-confidence.png",
+    title: "Self Confidence",
+  },
+  {
+    imgUrl: "./assets/perplexed-mind.png",
+    title: "Perplexed Mind",
+  },
+  {
+    imgUrl: "./assets/womens-rights.png",
+    title: "Womens right's",
+  },
+  {
+    imgUrl: "./assets/social-class.png",
+    title: "Social Class",
+  },
+];
