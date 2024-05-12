d305b7a329aa6f9319064db6dcae295aeea8d199
Allen Astral De Jesus <dj.allenastral@gmail.com>
2024-05-12 23:22:38 +0800

    podcast: add - about, testimonials, responsiveness

--- podcast.js
+++ podcast.js
@@ -1,4 +1,4 @@
-const jsonData = [
+const covers = [
   {
     imgUrl: "./assets/tesla-autopilot.png",
     title: "Tesla Autopilot",
@@ -18,5 +18,31 @@ const jsonData = [
   {
     imgUrl: "./assets/social-class.png",
     title: "Social Class",
+  },
+];
+
+const reviewers = [
+  {
+    review:
+      "Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. ",
+    avatar: "./assets/testimonial-1.png",
+    name: "Luna lovegood",
+    platformUrl: "./assets/support-spotify.png",
+  },
+
+  {
+    review:
+      "Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. ",
+    avatar: "./assets/testimonial-2.png",
+    name: "Emily Blunt",
+    platformUrl: "./assets/support-google-podcats.png",
+  },
+
+  {
+    review:
+      "Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. ",
+    avatar: "./assets/testimonial-3t.png",
+    name: "Mia Winters,",
+    platformUrl: "./assets/support-youtube.png",
   },
 ];
