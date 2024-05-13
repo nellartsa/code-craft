d2c2ea81b29c3b259a4a7f7bf47b2c1e5147a789
Allen Astral De Jesus <dj.allenastral@gmail.com>
2024-05-13 22:53:01 +0800

    podcast: add - testimonials/swiper, benefits, episodes, sponsor, articles

--- podcast.js
+++ podcast.js
@@ -35,14 +35,72 @@ const reviewers = [
       "Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. ",
     avatar: "./assets/testimonial-2.png",
     name: "Emily Blunt",
-    platformUrl: "./assets/support-google-podcats.png",
+    platformUrl: "./assets/support-google-podcast.png",
   },
 
   {
     review:
       "Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. ",
-    avatar: "./assets/testimonial-3t.png",
+    avatar: "./assets/testimonial-3.png",
     name: "Mia Winters,",
     platformUrl: "./assets/support-youtube.png",
+  },
+];
+
+const episodes = [
+  {
+    epi: "Eps. 1",
+    title: "Are you a Perplexed Mind Person?",
+    imgUrl: "./assets/perplexed-mind.png",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
+    type: ["mind-behavior", "health"],
+    host: {
+      first: "./assets/host-1.png",
+      second: "./assets/host-2.png",
+    },
+  },
+  {
+    epi: "Eps. 2",
+    title: "Type of Social Classes of People",
+    imgUrl: "./assets/social-class.png",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
+    type: ["social class", "wealth"],
+    host: {
+      first: "./assets/host-1.png",
+      second: "./assets/host-2.png",
+    },
+  },
+  {
+    epi: "Eps. 3",
+    title: "How to Deal with Self-Confidence",
+    imgUrl: "./assets/self-confidence.png",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
+    type: ["self-esteem", "health"],
+    host: {
+      first: "./assets/host-1.png",
+      second: "./assets/host-2.png",
+    },
+  },
+  {
+    epi: "Eps. 4",
+    title: "Women's Rights? Is it alright?",
+    imgUrl: "./assets/womens-rights.png",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
+    type: ["women's rights"],
+    host: {
+      first: "./assets/host-1.png",
+      second: "./assets/host-2.png",
+    },
+  },
+  {
+    epi: "Eps. 5",
+    title: "Tesla Autopilot Controversy",
+    imgUrl: "./assets/tesla-autopilot.png",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
+    type: ["automation", "tech"],
+    host: {
+      first: "./assets/host-1.png",
+      second: "./assets/host-2.png",
+    },
   },
 ];
