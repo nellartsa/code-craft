209efe433d54dc840b7b8a4208ca32e22b0d590e
Allen Astral De Jesus <dj.allenastral@gmail.com>
2024-05-21 11:31:07 +0800

    podcast: asset imgs to own dir | foodieland: add asset imgs

--- podcast.js
+++ podcast.js
@@ -1,22 +1,22 @@
 const covers = [
   {
-    imgUrl: "./assets/tesla-autopilot.png",
+    imgUrl: "./assets/podcast/tesla-autopilot.png",
     title: "Tesla Autopilot",
   },
   {
-    imgUrl: "./assets/self-confidence.png",
+    imgUrl: "./assets/podcast/self-confidence.png",
     title: "Self Confidence",
   },
   {
-    imgUrl: "./assets/perplexed-mind.png",
+    imgUrl: "./assets/podcast/perplexed-mind.png",
     title: "Perplexed Mind",
   },
   {
-    imgUrl: "./assets/womens-rights.png",
+    imgUrl: "./assets/podcast/womens-rights.png",
     title: "Womens right's",
   },
   {
-    imgUrl: "./assets/social-class.png",
+    imgUrl: "./assets/podcast/social-class.png",
     title: "Social Class",
   },
 ];
@@ -25,25 +25,25 @@ const reviewers = [
   {
     review:
       "Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. ",
-    avatar: "./assets/testimonial-1.png",
+    avatar: "./assets/podcast/testimonial-1.png",
     name: "Luna lovegood",
-    platformUrl: "./assets/support-spotify.png",
+    platformUrl: "./assets/podcast/support-spotify.png",
   },
 
   {
     review:
       "Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. ",
-    avatar: "./assets/testimonial-2.png",
+    avatar: "./assets/podcast/testimonial-2.png",
     name: "Emily Blunt",
-    platformUrl: "./assets/support-google-podcast.png",
+    platformUrl: "./assets/podcast/support-google-podcast.png",
   },
 
   {
     review:
       "Lorem ipsum dolor sit amet consectet piscing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. ",
-    avatar: "./assets/testimonial-3.png",
+    avatar: "./assets/podcast/testimonial-3.png",
     name: "Mia Winters,",
-    platformUrl: "./assets/support-youtube.png",
+    platformUrl: "./assets/podcast/support-youtube.png",
   },
 ];
 
@@ -52,54 +52,54 @@ const episodes = [
     id: 1,
     epi: "Eps. 1",
     title: "Are you a Perplexed Mind Person?",
-    imgUrl: "./assets/perplexed-mind.png",
+    imgUrl: "./assets/podcast/perplexed-mind.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
     types: ["mind-behavior", "health"],
-    hosts: ["./assets/host-1.png", "./assets/host-2.png"],
+    hosts: ["./assets/podcast/host-1.png", "./assets/podcast/host-2.png"],
   },
   {
     id: 2,
     epi: "Eps. 2",
     title: "Type of Social Classes of People",
-    imgUrl: "./assets/social-class.png",
+    imgUrl: "./assets/podcast/social-class.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
     types: ["social class", "wealth"],
-    hosts: ["./assets/host-1.png", "./assets/host-2.png"],
+    hosts: ["./assets/podcast/host-1.png", "./assets/podcast/host-2.png"],
   },
   {
     id: 3,
     epi: "Eps. 3",
     title: "How to Deal with Self-Confidence",
-    imgUrl: "./assets/self-confidence.png",
+    imgUrl: "./assets/podcast/self-confidence.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
     types: ["self-esteem", "health"],
-    hosts: ["./assets/host-1.png", "./assets/host-2.png"],
+    hosts: ["./assets/podcast/host-1.png", "./assets/podcast/host-2.png"],
   },
   {
     id: 4,
     epi: "Eps. 4",
     title: "Women's Rights? Is it alright?",
-    imgUrl: "./assets/womens-rights.png",
+    imgUrl: "./assets/podcast/womens-rights.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
     types: ["women's rights"],
-    hosts: ["./assets/host-1.png", "./assets/host-2.png"],
+    hosts: ["./assets/podcast/host-1.png", "./assets/podcast/host-2.png"],
   },
   {
     id: 5,
     epi: "Eps. 5",
     title: "Tesla Autopilot Controversy",
-    imgUrl: "./assets/tesla-autopilot.png",
+    imgUrl: "./assets/podcast/tesla-autopilot.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
     types: ["automation", "tech"],
-    hosts: ["./assets/host-1.png", "./assets/host-2.png"],
+    hosts: ["./assets/podcast/host-1.png", "./assets/podcast/host-2.png"],
   },
   {
     id: 6,
     epi: "Eps. 6",
     title: "Pandemic Becoming Endemic",
-    imgUrl: "./assets/pandemic.png",
+    imgUrl: "./assets/podcast/pandemic.png",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.",
     types: ["covid-19", "health"],
-    hosts: ["./assets/host-1.png", "./assets/host-2.png"],
+    hosts: ["./assets/podcast/host-1.png", "./assets/podcast/host-2.png"],
   },
 ];
